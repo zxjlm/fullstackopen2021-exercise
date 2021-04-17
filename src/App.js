4a8b29d37721e009d2aed45d7ba5b3eb2c3fadc1
harumonia <zxjlm233@gmail.com>
2021-04-17 18:07:16 +0800

    feat: 1.12 finished

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React from "react";
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+import React, { useState } from "react";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+
+  const changeSeletedHandler = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
   };
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content part={part1}></Content>
-      <Content part={part2}></Content>
-      <Content part={part3}></Content>
-
-      <Total total_exer={part1.exercises + part2.exercises + part3.exercises}>
-        Number of exercises
-      </Total>
+      <div>{anecdotes[selected]}</div>
+      <button onClick={changeSeletedHandler}>next anecdote</button>
     </div>
   );
 };
