582f5cc30cefcfbac8d41e2b8c628666b254da32
harumonia <zxjlm233@gmail.com>
2021-04-18 09:24:58 +0800

    pre: middle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import Filter from "./components/Filter";
 
 const App = () => {
-  return <div>9527</div>;
+  const [conuntries, setConuntries] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/#api-endpoints-all")
+      .then((response) => setConuntries(response.data));
+  });
+
+  return (
+    <div>
+      <Filter setSearchText={setSearchText}></Filter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Filter.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Filter = ({ setSearchText }) => {
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="seatch-panel">find countries</label>
+      <input onChange={handleSearchChange}></input>
+    </div>
+  );
+};
