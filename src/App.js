897a6af2591d1e4571c2ff918c920fd4b50b1c61
harumonia <zxjlm233@gmail.com>
2021-04-18 09:02:16 +0800

    feat: 2.11 finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Person from "./components/Person";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
-  const [personToShow, setPersonToShow] = useState(persons);
+  const [persons, setPersons] = useState([]);
+  const [personToShow, setPersonToShow] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      setPersonToShow(response.data);
+    });
+  }, []);
 
   return (
     <div>
