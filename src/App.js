12d48b7515263a96efb3e6c7b983812759e9c20d
harumonia <zxjlm233@gmail.com>
2021-04-18 09:04:15 +0800

    fix: clean main branch

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 import React from "react";
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-
-  return (
-    <div>
-      <Header course={course}></Header>
-      <Content part={part1}></Content>
-      <Content part={part2}></Content>
-      <Content part={part3}></Content>
-
-      <Total total_exer={part1.exercises + part2.exercises + part3.exercises}>
-        Number of exercises
-      </Total>
-    </div>
-  );
+  return <div>9527</div>;
 };
 
 export default App;
--- src/Total.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Total = (props) => {
-  return <p>{props.total_exer}</p>;
-};
-
-export default Total;
